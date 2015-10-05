1018e8ad8f5239bd42ad6e6e10852f4aaee06261
jeremyleebrooks <jeremybrooks321@gmail.com>
2015-10-05 12:35:51 +0100

    Basic spring configuration with 2 beans using xml

--- src/main/java/com/spring/fundamentals/Application.java
+++ src/main/java/com/spring/fundamentals/Application.java
@@ -2,13 +2,19 @@ package com.spring.fundamentals;
 
 import com.spring.fundamentals.service.CustomerService;
 import com.spring.fundamentals.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by jbrooks on 05/10/2015.
  */
 public class Application {
     public static void main(String[] args){
-        CustomerService service = new CustomerServiceImpl();
+//        CustomerService service = new CustomerServiceImpl();
+
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
         System.out.println(service.findAll().get(0).getFirstName() + " " + service.findAll().get(0).getLastName());
     }
 }
--- src/main/java/com/spring/fundamentals/service/CustomerServiceImpl.java
+++ src/main/java/com/spring/fundamentals/service/CustomerServiceImpl.java
@@ -2,7 +2,6 @@ package com.spring.fundamentals.service;
 
 import com.spring.fundamentals.model.Customer;
 import com.spring.fundamentals.repository.CustomerRepo;
-import com.spring.fundamentals.repository.HibernateCustomerRepoImpl;
 
 import java.util.List;
 
@@ -10,9 +9,14 @@ import java.util.List;
  * Created by jbrooks on 05/10/2015.
  */
 public class CustomerServiceImpl implements CustomerService {
-    private CustomerRepo customerRepo = new HibernateCustomerRepoImpl();
+
+    private CustomerRepo customerRepo;
 
     public List<Customer> findAll() {
         return customerRepo.findAll();
+    }
+
+    public void setCustomerRepo(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
     }
 }
