61d63e62206a9d30e02a80db49dca8bca4de1924
jeremyleebrooks <jeremybrooks321@gmail.com>
2015-10-06 11:51:30 +0100

    Annotations pure java with complete autowiring, nice!

--- src/main/java/com/spring/fundamentals/AppConfig.java
+++ src/main/java/com/spring/fundamentals/AppConfig.java
@@ -1,11 +1,6 @@
 package com.spring.fundamentals;
 
-import com.spring.fundamentals.repository.CustomerRepo;
-import com.spring.fundamentals.repository.HibernateCustomerRepoImpl;
-import com.spring.fundamentals.service.CustomerService;
-import com.spring.fundamentals.service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -14,16 +9,4 @@ import org.springframework.context.annotation.ComponentScan;
 @Configurable
 @ComponentScan({"com.spring.fundamentals"})
 public class AppConfig {
-
-//    @Bean(name = "customerService")
-//    public CustomerService getCustomerService(){
-//        CustomerServiceImpl customerService = new CustomerServiceImpl();
-//
-//        return customerService;
-//    }
-
-//    @Bean(name = "customerRepo")
-//    public CustomerRepo getCustomerRepo(){
-//        return new HibernateCustomerRepoImpl();
-//    }
 }
