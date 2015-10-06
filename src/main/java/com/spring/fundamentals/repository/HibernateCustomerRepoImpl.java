99a6a9adf6fbc86cd775f8d895517fc6dfe11481
jeremyleebrooks <jeremybrooks321@gmail.com>
2015-10-06 13:27:47 +0100

    Created an external property file, injected it into the xml spring config and used it to set a value in a class

--- src/main/java/com/spring/fundamentals/repository/HibernateCustomerRepoImpl.java
+++ src/main/java/com/spring/fundamentals/repository/HibernateCustomerRepoImpl.java
@@ -1,6 +1,7 @@
 package com.spring.fundamentals.repository;
 
 import com.spring.fundamentals.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -12,11 +13,15 @@ import java.util.List;
 
 @Repository("customerRepo")
 public class HibernateCustomerRepoImpl implements CustomerRepo {
+
+    @Value("${testExtProperty}")
+    private String testExtProperty;
+
     public List<Customer> findAll() {
         List<Customer> customers = new ArrayList<Customer>();
 
         Customer customer = new Customer();
-        customer.setFirstName("Jeremy");
+        customer.setFirstName(testExtProperty);
         customer.setLastName("Brooks");
 
         customers.add(customer);
