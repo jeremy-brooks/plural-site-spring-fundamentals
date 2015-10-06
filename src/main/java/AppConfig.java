ff1b47e973669a0ece0f1146cf65780064f64f21
jeremyleebrooks <jeremybrooks321@gmail.com>
2015-10-06 11:17:58 +0100

    new app config

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,6 +1,17 @@
+import com.spring.fundamentals.service.CustomerService;
+import com.spring.fundamentals.service.CustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
 /**
  * Created by jbrooks on 06/10/2015.
  */
+@Configurable
 public class AppConfig {
+
+    @Bean(name = "customerService")
+    public CustomerService getCustomerService(){
+        return new CustomerServiceImpl();
+    }
 
 }
